a5bde1cb525cd35144153da1a8e27235b1b7f346
Ahamad-PC <ahamad.jedu@gmail.com>
2018-05-20 16:15:10 +0700

    5.4-การใช้งาน Redux

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,36 @@ import React, {Component} from 'react';
 
 class Home extends Component {
 
+    state = {
+        age: 0
+    }
+
     render() {
         return (
             <div>
                 <h1>Page Home</h1>
+                <h3>อายุของคุณ : {this.state.age}</h3>
+                <button onClick={this.increteAge}> + คลิกบวกอายุ</button> &nbsp;
+                <button onClick={this.decreteAge}> - คลิกบวกอายุ</button>
+
             </div>
         )
+    }
+
+    increteAge = () => {
+        this.setState((prevSate) => {
+            return {
+                age: prevSate.age + 1
+            }
+        })
+    }
+
+    decreteAge = () => {
+        this.setState((prevSate) => {
+            return {
+                age: prevSate.age - 1
+            }
+        })
     }
 
 }
