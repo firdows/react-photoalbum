c0a688a88264532d49fed2ff0adc93a44853e89a
Ahamad-PC <ahamad.jedu@gmail.com>
2018-05-21 00:11:51 +0700

    5.7-create component User

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,11 +1,26 @@
 import React, {Component} from 'react';
+import UserList from './Users/UserList';
 
 class User extends Component {
+    state = {
+        data: null,
+        isLoading: false
+    }
+
+    componentDidMount() {
+        this.setState({isLoading: true});
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(result => result.json())
+            .then(result => this.setState({data: result, isLoading: false}))
+    }
 
     render() {
+        const {data, isLoading} = this.state;
         return (
             <div>
                 <h1>Page User</h1>
+                {isLoading && <div>Loading...</div>}
+                <UserList data={data}/>
             </div>
         )
     }
