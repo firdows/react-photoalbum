2a3c85a16df79b97683e47a02107dd5877533914
Ahamad-PC <ahamad.jedu@gmail.com>
2018-05-20 23:33:23 +0700

    5.6-ติดตั้งและใช้ redux

--- src/index.js
+++ src/index.js
@@ -5,7 +5,29 @@ import registerServiceWorker from './registerServiceWorker';
 
 import {Router, browserHistory} from 'react-router';
 import routes from './routes';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux'
+
+function countAge(state = 0, action) {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state
+    }
+}
+
+const store = createStore(countAge)
+store.subscribe(() => {
+    console.log('subscribe', store.getState())
+})
+console.log('befare dispatch', store.getState())
+store.dispatch({type: 'INCREMENT'})
 
 ReactDOM.render(
-    <Router history={browserHistory} routes={routes}/>, document.getElementById('root'));
+    <Provider store={store}>
+    <Router history={browserHistory} routes={routes}/>
+</Provider>, document.getElementById('root'));
 registerServiceWorker();
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,39 +1,46 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
 class Home extends Component {
 
-    state = {
-        age: 0
-    }
+    // state = {     age: 0 }
 
     render() {
         return (
             <div>
                 <h1>Page Home</h1>
-                <h3>อายุของคุณ : {this.state.age}</h3>
-                <button onClick={this.increteAge}> + คลิกบวกอายุ</button> &nbsp;
-                <button onClick={this.decreteAge}> - คลิกบวกอายุ</button>
+                <h3>อายุของคุณ : {this.props.age}</h3>
+                <button onClick={this.props.increteAge}>
+                    + คลิกบวกอายุ</button>
+                &nbsp;
+                <button onClick={this.props.decreteAge}>
+                    - คลิกบวกอายุ</button>
 
             </div>
         )
     }
 
-    increteAge = () => {
-        this.setState((prevSate) => {
-            return {
-                age: prevSate.age + 1
-            }
-        })
-    }
-
-    decreteAge = () => {
-        this.setState((prevSate) => {
-            return {
-                age: prevSate.age - 1
-            }
-        })
-    }
+    // increteAge = () => {     this.setState((prevSate) => {         return { age:
+    // prevSate.age + 1         }     }) } decreteAge = () => {
+    // this.setState((prevSate) => {         return {             age: prevSate.age
+    // - 1         }     }) }
 
 }
 
-export default Home
+function mapStateToProps(state) {
+    return {age: state}
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        increteAge: () => {
+            dispatch({type: 'INCREMENT'})
+        },
+        decreteAge: () => {
+            dispatch({type: 'DECREMENT'})
+        }
+    }
+}
+
+//export default Home
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
