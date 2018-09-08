0ff284b1687929e38f7141c7fce5e7bb50d75a54
Ahamad-PC <ahamad.jedu@gmail.com>
2018-09-08 21:07:16 +0700

    add menu left on layout

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import 'bulma/css/bulma.css';
 import Header from '../components/layouts/Header';
+import Left from '../components/layouts/Left'
 
 class App extends Component {
     render() {
@@ -9,7 +10,15 @@ class App extends Component {
                 <Header/>
                 <div className="container">
                     <div className="content">
-                        {this.props.children}
+                        <div class="columns">
+                            <div class="column is-3">
+                            <Left/>
+                            </div>
+                            <div class="column is-9">
+                                {this.props.children}
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </div>
